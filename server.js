e03d7caef7e889a1d56be74d0f8ab6a92a5fcbd4
BuilderClintz <wpcss001@gmail.com>
2024-07-05 08:31:50 -1100

    update

--- server.js
+++ server.js
@@ -5,8 +5,59 @@ const app = express()
 require("dotenv").config()
 require("./Config/dbConnect")
 
+app.post("/api/v1/user/register", (req,res)=>{
+    try {
+        res.json({
+            status: "success",
+            data: "User register successfully"
+        })
+    } catch (error) {
+        res.json(error.message)
+        
+    }
+})
 
-const PORT = process.env.PORT || 9000
+app.post("/api/v1/user/Login", (req,res)=>{
+    try {
+        res.json({
+            status: "success",
+            data: "User Login successfully"
+        })
+    } catch (error) {
+        res.json(error.message)
+        
+    }
+})
+
+app.post("/api/v1/user/Update", (req,res)=>{
+    try {
+        res.json({
+            status: "success",
+            data: "User Updated successfully"
+        })
+    } catch (error) {
+        res.json(error.message)
+        
+    }
+})
+
+app.delete("/api/v1/user/:id", async (req, res)=> {
+    try {
+        res.json({
+            status: "success",
+            data: "User deleted successfully"
+        })
+    } catch (error) {
+        res.json(error.message)
+        
+    }
+})
+
+
+
+
+
+const PORT = process.env.PORT || 8000
 app.listen(PORT, console.log(`server is running on ${PORT}`))
 
 
