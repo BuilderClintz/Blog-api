0cd7b2b510d7b5908dfaf1245c136091e8e3b5df
BuilderClintz <wpcss001@gmail.com>
2024-08-06 11:00:51 +0100

    update database

--- Config/dbConnect.js
+++ Config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const dbconnect = async() => {
     try{
         await mongoose.connect(process.env.MONGODB_URL)
-        console.log("Database connected successfully")
+        console.log("Database established")
     }catch(error){
         console.log(error.message)
         process.exit(1)
